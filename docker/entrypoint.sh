f2ce618215beb3c29e7f27a9b9ac91a1cd1acfdc
Dario Todoroski <todoroski.a24133@next.edu.mk>
2026-06-21 17:10:05 +0200

    Switch Apache to prefork in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -42,4 +42,19 @@ if [ "${RUN_MIGRATIONS:-true}" = "true" ]; then
     php artisan migrate --force --ansi
 fi
 
+if [ "${1:-}" = "apache2-foreground" ]; then
+    rm -f \
+        /etc/apache2/mods-enabled/mpm_event.load \
+        /etc/apache2/mods-enabled/mpm_event.conf \
+        /etc/apache2/mods-enabled/mpm_worker.load \
+        /etc/apache2/mods-enabled/mpm_worker.conf
+
+    ln -sf ../mods-available/mpm_prefork.load /etc/apache2/mods-enabled/mpm_prefork.load
+    if [ -f /etc/apache2/mods-available/mpm_prefork.conf ]; then
+        ln -sf ../mods-available/mpm_prefork.conf /etc/apache2/mods-enabled/mpm_prefork.conf
+    fi
+
+    apache2ctl configtest
+fi
+
 exec "$@"
