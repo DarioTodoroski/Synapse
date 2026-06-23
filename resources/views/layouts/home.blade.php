9eff1b00bb7a512ae02a71dd2ce6205a70a9cf1e
Dario Todoroski <todoroski.a24133@next.edu.mk>
2026-06-23 18:33:12 +0200

    Add favicon to admin and layout pages

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Synapse HR</title>
+    <link rel="icon" type="image/jpeg" href="{{ asset('img/synapse-logo.jpg') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
